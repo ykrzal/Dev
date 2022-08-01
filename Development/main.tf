93d1b2636437184b55b2e722c1b9c6ad53b4b94b
ykrzal <92915654+ykrzal@users.noreply.github.com>
2022-08-01 14:11:33 +0300

    Update main.tf

--- Development/main.tf
+++ Development/main.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.23.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "4.23.0"
+#     }
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-2"
+# provider "aws" {
+#   region = "us-east-2"
   
-}
+# }
 
-module "templates" {
-  source = "./templates"
-}
+# module "templates" {
+#   source = "./templates"
+# }
