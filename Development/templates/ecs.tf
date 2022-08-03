ca203ecd307763e66c01328a633e8daa15504bd5
yurii <ykrzal@gmail.com>
2022-08-03 13:38:16 +0300

    new fix

--- Development/templates/ecs.tf
+++ Development/templates/ecs.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "admin" {
   #task_role_arn = aws_iam_role.admin_role.arn
   task_role_arn = aws_iam_role.ecs_tasks_execution_role.arn
 
-  container_definitions = jsonencode(
+  container_definitions = <<TASK_DEFINITION
 [
   {
     "name": "${var.environment}-boopos-admin",
@@ -81,9 +81,34 @@ resource "aws_ecs_task_definition" "admin" {
     }
   }
 ]
-)
+TASK_DEFINITION
 
 }
+
+
+# resource "aws_ecs_task_definition" "test" {
+#   family                   = "test"
+#   requires_compatibilities = ["FARGATE"]
+#   network_mode             = "awsvpc"
+#   cpu                      = 1024
+#   memory                   = 2048
+#   container_definitions    = <<TASK_DEFINITION
+# [
+#   {
+#     "name": "iis",
+#     "image": "mcr.microsoft.com/windows/servercore/iis",
+#     "cpu": 1024,
+#     "memory": 2048,
+#     "essential": true
+#   }
+# ]
+# TASK_DEFINITION
+
+#   runtime_platform {
+#     operating_system_family = "WINDOWS_SERVER_2019_CORE"
+#     cpu_architecture        = "X86_64"
+#   }
+# }
 
 resource "aws_ecs_service" "admin" {
   name            = "${var.environment}-boopos-admin"
