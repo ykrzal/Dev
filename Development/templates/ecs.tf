72ad58bf3c2f07f348697b0029b7fb166df35bb5
yurii <ykrzal@gmail.com>
2022-08-03 14:39:18 +0300

    new fix

--- Development/templates/ecs.tf
+++ Development/templates/ecs.tf
@@ -36,8 +36,7 @@ resource "aws_ecs_task_definition" "admin" {
   #task_role_arn = aws_iam_role.admin_role.arn
   task_role_arn = aws_iam_role.ecs_tasks_execution_role.arn
 
-  container_definitions = <<TASK_DEFINITION
-[
+  container_definitions = jsonencode([
   {
     "name": "${var.environment}-boopos-admin",
     "image": "${var.admin_image}",
@@ -45,8 +44,8 @@ resource "aws_ecs_task_definition" "admin" {
     "portMappings": [
       {
         "protocol": "tcp",
-        "containerPort": ${jsonencode(var.admin_container_port)},
-        "hostPort": ${jsonencode(var.admin_container_port)}
+        "containerPort": "${var.admin_container_port}"
+        "hostPort": "${var.admin_container_port}"
       }
     ],
     "environment": [
@@ -80,35 +79,10 @@ resource "aws_ecs_task_definition" "admin" {
       }
     }
   }
-]
-TASK_DEFINITION
 
+])
 }
 
-
-# resource "aws_ecs_task_definition" "test" {
-#   family                   = "test"
-#   requires_compatibilities = ["FARGATE"]
-#   network_mode             = "awsvpc"
-#   cpu                      = 1024
-#   memory                   = 2048
-#   container_definitions    = <<TASK_DEFINITION
-# [
-#   {
-#     "name": "iis",
-#     "image": "mcr.microsoft.com/windows/servercore/iis",
-#     "cpu": 1024,
-#     "memory": 2048,
-#     "essential": true
-#   }
-# ]
-# TASK_DEFINITION
-
-#   runtime_platform {
-#     operating_system_family = "WINDOWS_SERVER_2019_CORE"
-#     cpu_architecture        = "X86_64"
-#   }
-# }
 
 resource "aws_ecs_service" "admin" {
   name            = "${var.environment}-boopos-admin"
@@ -127,6 +101,6 @@ resource "aws_ecs_service" "admin" {
   load_balancer {
     target_group_arn = aws_alb_target_group.admin.id
     container_name   = "${var.environment}-boopos-admin"
-    container_port   = var.admin_port
+    container_port   =  var.admin_container_port
   }
 }
