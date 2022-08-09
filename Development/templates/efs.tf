3a26a91b2d0f31a451ffd1ce59526720fb0194c6
ykrzal <92915654+ykrzal@users.noreply.github.com>
2022-08-09 17:24:51 +0300

    Update efs.tf

--- Development/templates/efs.tf
+++ Development/templates/efs.tf
@@ -8,7 +8,7 @@ resource "aws_efs_mount_target" "admin_files" {
   count               = length(var.private_admin)
   
   file_system_id  = aws_efs_file_system.admin_files.id
-  subnet_id       = var.private_subnets[count.index]
+  subnet_id       = var.private_admin[count.index]
   security_groups = aws_security_group.admin_site_efs.id
   
 }
