394dab538ad969d0e17fac0475f98825b97cd004
ykrzal <92915654+ykrzal@users.noreply.github.com>
2022-08-12 16:44:25 +0300

    Update codepipeline.tf

--- Development/templates/codepipeline.tf
+++ Development/templates/codepipeline.tf
@@ -49,7 +49,7 @@ resource "aws_codebuild_project" "codebuild_project_admin_site" {
 
     s3_logs {
       status   = "ENABLED"
-      location = "${aws_s3_bucket.s3_logging_bucket}/admin-build-log"
+      location = "aws_s3_bucket.s3_logging_bucket"
     }
   }
 
@@ -95,7 +95,7 @@ resource "aws_codebuild_project" "codebuild_deploy_admin_site" {
 
     s3_logs {
       status   = "ENABLED"
-      location = "${aws_s3_bucket.s3_logging_bucket}/admin-build-log"
+      location = "aws_s3_bucket.s3_logging_bucket"
     }
   }
 
