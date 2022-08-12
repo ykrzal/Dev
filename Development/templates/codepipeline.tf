454ac6ab0e350a8682b1cccded250628d119011d
ykrzal <92915654+ykrzal@users.noreply.github.com>
2022-08-12 16:38:06 +0300

    Update codepipeline.tf

--- Development/templates/codepipeline.tf
+++ Development/templates/codepipeline.tf
@@ -18,7 +18,7 @@ resource "aws_codebuild_project" "codebuild_project_admin_site" {
   name          = "${var.environment}-build-admin-site"
   description   = "Build codebuild project"
   build_timeout = "5"
-  service_role  = "aws_iam_role.codebuild_role.arn"
+  service_role  = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
