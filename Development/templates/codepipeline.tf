7d15c462c96bd372101f3a6bccbe3ede381fe928
ykrzal <92915654+ykrzal@users.noreply.github.com>
2022-08-12 16:14:10 +0300

    Update codepipeline.tf

--- Development/templates/codepipeline.tf
+++ Development/templates/codepipeline.tf
@@ -18,7 +18,7 @@ resource "aws_codebuild_project" "codebuild_project_admin_site" {
   name          = "${var.environment}-build-admin-site"
   description   = "Build codebuild project"
   build_timeout = "5"
-  service_role  = aws_iam_role.codebuild_role
+  service_role  = "aws_iam_role.codebuild_role"
 
   artifacts {
     type = "CODEPIPELINE"
@@ -26,7 +26,7 @@ resource "aws_codebuild_project" "codebuild_project_admin_site" {
 
   cache {
     type     = "S3"
-    location = aws_s3_bucket.s3_logging_bucket
+    location = "aws_s3_bucket.s3_logging_bucket"
   }
 
   environment {
@@ -64,7 +64,7 @@ resource "aws_codebuild_project" "codebuild_deploy_admin_site" {
   name          = "${var.environment}-deploy-admin-site"
   description   = "Deploy codebuild project"
   build_timeout = "5"
-  service_role  = aws_iam_role.codebuild_role
+  service_role  = "aws_iam_role.codebuild_role"
 
   artifacts {
     type = "CODEPIPELINE"
@@ -72,7 +72,7 @@ resource "aws_codebuild_project" "codebuild_deploy_admin_site" {
 
   cache {
     type     = "S3"
-    location = aws_s3_bucket.s3_logging_bucket
+    location = "aws_s3_bucket.s3_logging_bucket"
   }
 
   environment {
@@ -111,7 +111,7 @@ resource "aws_codepipeline" "codepipeline_admin_site" {
   role_arn = aws_iam_role.codepipeline_role
 
   artifact_store {
-    location = aws_s3_bucket.codepipeline_artifact_bucket
+    location = "aws_s3_bucket.codepipeline_artifact_bucket"
     type     = "S3"
   }
 
