bfa27451094390068417924415aaee34c30148c0
ethtol <yurii.martyniuk@techmagic.co>
2022-08-13 15:13:44 +0300

    ads

--- Development/templates/codepipeline.tf
+++ Development/templates/codepipeline.tf
@@ -60,7 +60,7 @@ resource "aws_codebuild_project" "codebuild_project_admin_site" {
   
   vpc_config {
     vpc_id             = "aws_vpc.main_vpc.id"
-    subnets            = [aws_subnet.public.*.id[0], aws_subnet.public.*.id[1]]
+    subnets            = "[aws_subnet.public.*.id[0], aws_subnet.public.*.id[1]]"
     security_group_ids = "aws_security_group.codebuild.id"
 
 }
@@ -113,7 +113,7 @@ resource "aws_codebuild_project" "codebuild_deploy_admin_site" {
 
   vpc_config {
     vpc_id             = "aws_vpc.main_vpc.id"
-    subnets            = [aws_subnet.public.*.id[0], aws_subnet.public.*.id[1]]
+    subnets            = "[aws_subnet.public.*.id[0], aws_subnet.public.*.id[1]]"
     security_group_ids = "aws_security_group.codebuild.id"
 
 }
