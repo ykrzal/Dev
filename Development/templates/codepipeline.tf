e26173ffcb1b2e9211ae9bcff9e01858fe603620
ethtol <yurii.martyniuk@techmagic.co>
2022-08-13 16:51:01 +0300

    ss

--- Development/templates/codepipeline.tf
+++ Development/templates/codepipeline.tf
@@ -56,7 +56,7 @@ resource "aws_codebuild_project" "codebuild_project_admin_site" {
   file_system_locations {
     type = "EFS"
     identifier = "GREEN"
-    location = "aws_efs_mount_target.admin_files_green"
+    location = "${aws_efs_mount_target.admin_files_green}"
     mount_point = "/mnt/efs"
   }
 
@@ -116,7 +116,7 @@ resource "aws_codebuild_project" "codebuild_deploy_admin_site" {
   file_system_locations {
     type = "EFS"
     identifier = "BLUE"
-    location = "aws_efs_mount_target.admin_files_blue"
+    location = "${aws_efs_mount_target.admin_files_blue}"
     mount_point = "/mnt/efs"
   }
 
