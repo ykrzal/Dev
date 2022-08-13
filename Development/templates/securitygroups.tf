d10a9a2704e32cfd67aed97b65ab616c0567c3c7
ethtol <yurii.martyniuk@techmagic.co>
2022-08-13 15:53:53 +0300

    sds

--- Development/templates/securitygroups.tf
+++ Development/templates/securitygroups.tf
@@ -138,7 +138,6 @@ resource "aws_security_group" "codebuild" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
