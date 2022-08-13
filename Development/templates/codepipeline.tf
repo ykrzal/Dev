7c101557207da19fc297e8c35ef5fd23ca52b12c
ethtol <yurii.martyniuk@techmagic.co>
2022-08-13 15:06:49 +0300

    ddd

--- Development/templates/codepipeline.tf
+++ Development/templates/codepipeline.tf
@@ -100,9 +100,9 @@ resource "aws_codebuild_project" "codebuild_deploy_admin_site" {
   }
 
     vpc_config = {
-    vpc_id             = "aws_vpc.main_vpc.id"
-    subnets            = [aws_subnet.public.*.id[0], aws_subnet.public.*.id[1]]
-    security_group_ids = "aws_security_group.codebuild.id"
+      vpc_id             = "aws_vpc.main_vpc.id"
+      subnets            = [aws_subnet.public.*.id[0], aws_subnet.public.*.id[1]]
+      security_group_ids = "aws_security_group.codebuild.id"
 
   }
 
