47c76c11889e15e47218f6005e8c1280e8b6ff30
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-16 16:32:00 +0300

    Vault vars add

--- Development/policies.tf
+++ Development/policies.tf
@@ -1,11 +1,10 @@
-# # Create admin policy in the root namespace
 resource "vault_policy" "admin_policy" {
   name   = "admins"
-  policy = file("Development/policies/admin-policy.hcl")
+  policy = file("policies/admin-policy.hcl")
 }
 
-# # Create 'training' policy
-# resource "vault_policy" "eaas-client" {
-#   name   = "eaas-client"
-#   policy = file("Development/policies/eaas-client-policy.hcl")
-# }
+# Create 'training' policy
+resource "vault_policy" "eaas-client" {
+  name   = "eaas-client"
+  policy = file("policies/eaas-client-policy.hcl")
+}
