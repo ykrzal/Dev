06df3781f19eb9b6ae311570703beab0006c03e3
ykrzal <92915654+ykrzal@users.noreply.github.com>
2022-08-16 18:06:59 +0300

    Update route53.tf

--- Development/templates/route53.tf
+++ Development/templates/route53.tf
@@ -1,3 +1,12 @@
 resource "aws_route53_zone" "zoom_dns" {
   name = "zoom.dns"
 }
+
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.zoom_dns.zone_id
+  name    = "test3.zoom.dns"
+  type    = "A"
+  ttl     = 300
+  records = 192.168.1.1
+}
