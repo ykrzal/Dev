85978361d5252f23e190821f653fdcefa79a43df
ethtol <yurii.martyniuk@techmagic.co>
2022-08-16 22:48:37 +0300

    sds

--- Development/policies.tf
+++ Development/policies.tf
@@ -1,10 +1,10 @@
-resource "vault_policy" "admin_policy" {
-  name   = "admins"
-  policy = file("policies/admin-policy.hcl")
-}
+# resource "vault_policy" "admin_policy" {
+#   name   = "admins"
+#   policy = file("policies/admin-policy.hcl")
+# }
 
-# Create 'training' policy
-resource "vault_policy" "eaas-client" {
-  name   = "eaas-client"
-  policy = file("policies/eaas-client-policy.hcl")
-}
+# # Create 'training' policy
+# resource "vault_policy" "eaas-client" {
+#   name   = "eaas-client"
+#   policy = file("policies/eaas-client-policy.hcl")
+# }
