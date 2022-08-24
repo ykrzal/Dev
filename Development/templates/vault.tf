c43bef67013a0f08d0cf911db5ac957fdee22bcb
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-24 22:12:54 +0300

    public endpoint add

--- Development/templates/vault.tf
+++ Development/templates/vault.tf
@@ -8,5 +8,5 @@ resource "hcp_vault_cluster" "hcp_tf_vault" {
   hvn_id     = hcp_hvn.hcp_tf_hvn.hvn_id
   cluster_id = var.cluster_id
   tier       = var.tier
-  #public_endpoint = true
+  public_endpoint = true
 }
