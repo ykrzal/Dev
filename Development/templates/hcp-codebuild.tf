cf7ccb7766e070653c943bf1fa68ce08c2698764
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-29 23:40:22 +0300

    codebuild part add

--- Development/templates/hcp-codebuild.tf
+++ Development/templates/hcp-codebuild.tf
@@ -1,50 +1,71 @@
 resource "aws_codebuild_project" "hcp-codebuild" {
-  name          = "test-hcp-kv-project"
-  description   = "test_codebuild_project"
-  service_role  = aws_iam_role.codebuild_role.arn
+  name                        = "test-hcp-kv-project"
+  description                 = "test"
+  build_timeout               = "5"
+  service_role                = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
   }
 
-
+  cache {
+    type                        = "S3"
+    location                    = aws_s3_bucket.s3_logging_bucket.bucket
+  }
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
+    image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = "true"
 
     environment_variable {
       name                      = "VAULT_ADDR"
       value                     = hcp_vault_cluster.hcp_tf_vault.vault_private_endpoint_url
     }
-
     environment_variable {
       name                      = "VAULT_NAMESPACE"
       value                     = "admin"
     }
   }
 
+
   logs_config {
     cloudwatch_logs {
-      group_name                = "log-group-hcp-build"
-      stream_name               = "log-stream-hcp-build"
+      group_name                = "log-group"
+      stream_name               = "log-stream"
     }
 
+    s3_logs {
+      status                    = "ENABLED"
+      location                  = "${aws_s3_bucket.s3_logging_bucket.id}/codebuild-log"
+    }
+  }
 
   source {
-    type                        = "NO_SOURCE"
-    buildspec                   = "Development/templates/buildspec/buildspec_test_hcp_kv.yml"
+    type                        = "CODEPIPELINE"
+    buildspec = <<EOF
+    version: 0.2
+
+    phases:
+      install:
+        commands:
+          - sudo yum install -y yum-utils
+          - sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+          - sudo yum -y install vault
+      build:
+        commands:
+          - sudo setcap -r /usr/bin/vault
+          - vault login -method=aws role=hcp_aws_auth_role
+          - vault kv get -mount=test1 -field=key secret
+    EOF
   }
 
   vpc_config {
-    vpc_id                      = aws_vpc.main_vpc.id
+    vpc_id                    = aws_vpc.main_vpc.id
+    subnets                   = ["${aws_subnet.private_codebuild.*.id[0]}", "${aws_subnet.private_codebuild.*.id[1]}"]
+    security_group_ids        = [aws_security_group.codebuild.id]
 
-    subnets                     = ["${aws_subnet.private_codebuild.*.id[0]}", "${aws_subnet.private_codebuild.*.id[1]}"]
-
-    security_group_ids          = [aws_security_group.codebuild.id]
-    }
-
-  }
+}
 }
