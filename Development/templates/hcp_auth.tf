01fbddc44579169a7e96057ab7ac34fd4c7fe3a7
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-29 22:30:00 +0300

    aws role arn fix

--- Development/templates/hcp_auth.tf
+++ Development/templates/hcp_auth.tf
@@ -31,6 +31,7 @@ resource "vault_aws_auth_backend_role" "hcp_aws_access_role" {
   role                            = aws_iam_role.codebuild_role.name
   backend                         = vault_auth_backend.aws.path
   auth_type                       = "iam"
-  bound_iam_role_arns             = ["arn:aws:iam::198448550418:role/codebuild_role"]
+  bound_iam_role_arns             = [aws_iam_role.codebuild_role.arn]
+  bound_regions                   = ["us-west-2","us-east-1"]
   token_policies                  = ["admin"]
 }
