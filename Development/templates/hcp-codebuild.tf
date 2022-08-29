5246d1266bc091ae8d0ec43545b55817f309ce25
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-29 23:56:34 +0300

    codebuild part fix

--- Development/templates/hcp-codebuild.tf
+++ Development/templates/hcp-codebuild.tf
@@ -15,7 +15,7 @@ resource "aws_codebuild_project" "hcp-codebuild" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = "true"
