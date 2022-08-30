9f013ed7cffed49524ccf35a32f6292f434c4635
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-30 12:09:08 +0300

    sts role add

--- Development/templates/hcp_auth.tf
+++ Development/templates/hcp_auth.tf
@@ -20,16 +20,21 @@ resource "vault_auth_backend" "aws" {
   type = "aws"
 }
 
-resource "vault_aws_auth_backend_client" "aws_iam_user" {
+resource "vault_aws_auth_backend_sts_role" "hcp_role" {
   backend    = vault_auth_backend.aws.path
-  access_key = aws_iam_access_key.hcp_auth_key.id
-  secret_key = aws_iam_access_key.hcp_auth_key.secret
+  sts_role   = aws_iam_role.codebuild_role.arn
 }
 
-resource "vault_aws_auth_backend_role" "hcp_aws_access_role" {
-  role                            = "hcp_aws_auth_role"
-  backend                         = vault_auth_backend.aws.path
-  auth_type                       = "iam"
-  bound_iam_principal_arns        = ["${aws_iam_role.codebuild_role.arn}/*"] 
-  token_policies                  = ["admins"]
-}
+# resource "vault_aws_auth_backend_client" "aws_iam_user" {
+#   backend    = vault_auth_backend.aws.path
+#   access_key = aws_iam_access_key.hcp_auth_key.id
+#   secret_key = aws_iam_access_key.hcp_auth_key.secret
+# }
+
+# resource "vault_aws_auth_backend_role" "hcp_aws_access_role" {
+#   role                            = "hcp_aws_auth_role"
+#   backend                         = vault_auth_backend.aws.path
+#   auth_type                       = "iam"
+#   bound_iam_principal_arns        = ["${aws_iam_role.codebuild_role.arn}/*"] 
+#   token_policies                  = ["admins"]
+# }
