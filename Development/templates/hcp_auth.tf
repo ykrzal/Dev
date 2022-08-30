3a224ca1f32ab887eea650b19598d69c464dcc8a
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-30 11:36:39 +0300

    bound_iam_principal_arns fix

--- Development/templates/hcp_auth.tf
+++ Development/templates/hcp_auth.tf
@@ -31,6 +31,6 @@ resource "vault_aws_auth_backend_role" "hcp_aws_access_role" {
   role                            = "hcp_aws_auth_role"
   backend                         = vault_auth_backend.aws.path
   auth_type                       = "iam"
-  bound_iam_principal_arns        = ["${aws_iam_role.codebuild_role.arn}/*"] 
+  bound_iam_principal_arns        = ["arn:aws:iam::198448550418:role/codebuild_role/*"] 
   token_policies                  = ["admins"]
 }
