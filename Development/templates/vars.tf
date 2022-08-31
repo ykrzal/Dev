56992c013dc2ad20f8b83406c582b2db83386264
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-31 22:10:24 +0300

    cluster change

--- Development/templates/vars.tf
+++ Development/templates/vars.tf
@@ -170,7 +170,7 @@ variable "hvn_id" {
 variable "cluster_id" {
   description = "The ID of the HCP Vault cluster."
   type        = string
-  default     = "hashicorp-tf-vault-cluster"
+  default     = "hashicorp-tf-vault"
 }
 
 
