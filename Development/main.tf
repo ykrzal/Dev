a8199d68a3e0cd4b29b545723ebe071cbfb1b8e9
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-31 18:21:25 +0300

    token creation part add

--- Development/main.tf
+++ Development/main.tf
@@ -24,7 +24,6 @@ module "templates" {
 provider "vault" {
   # address =  hcp_vault_cluster.hcp_tf_vault.vault_public_endpoint_url
   # token   =  "$TF_TOKEN"
-  version = "~> 3.8.2"
 }
 
 provider "hcp" {}
