671503720463e7d33e88933fd0ac8f3f61fb1f66
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-08-31 15:37:29 +0300

    token creation part add

--- Development/templates/hcp_auth.tf
+++ Development/templates/hcp_auth.tf
@@ -29,7 +29,7 @@ resource "vault_terraform_cloud_secret_backend" "terraform" {
 resource "vault_terraform_cloud_secret_role" "terraform_role" {
   backend      = vault_terraform_cloud_secret_backend.terraform.backend
   name         = "terraform-role"
-  organization = "TerraCloudZoom"
+  organization = "${TF_ORGANIZATION}"
 }
 
 resource "vault_terraform_cloud_secret_creds" "token" {
