88e922f99fd5e5ff922761ef41fd269f122e238c
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-09-01 14:00:59 +0300

    hvn part

--- Development/templates/hcp_secrets.tf
+++ Development/templates/hcp_secrets.tf
@@ -1,8 +1,8 @@
-# # Enable K/V v2 secrets engine at 'kv-v2'
-# resource "vault_mount" "kv-v2" {
-#   path = "kv-v2"
-#   type = "kv-v2"
-# }
+# Enable K/V v2 secrets engine at 'kv-v2'
+resource "vault_mount" "test" {
+  path = "test"
+  type = "kv"
+}
 
 # # Enable Transit secrets engine at 'transit'
 # resource "vault_mount" "transit" {
