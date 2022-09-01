52f5bd6dbb155f849e2ecd242054b619f89e95ad
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-09-01 13:27:07 +0300

    hvn part

--- Development/templates/main.tf
+++ Development/templates/main.tf
@@ -1,4 +1,4 @@
 provider "vault" {
-    VAULT_ADDR    =  hcp_vault_cluster.hcp_tf_vault.vault_public_endpoint_url
-    VAULT_TOKEN      =  hcp_vault_cluster_admin_token.hcp_vault_admin_token.token
+    address   =  hcp_vault_cluster.hcp_tf_vault.vault_public_endpoint_url
+    token     =  hcp_vault_cluster_admin_token.hcp_vault_admin_token.id
 }
