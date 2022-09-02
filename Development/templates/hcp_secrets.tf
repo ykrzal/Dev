76570fab154bc910bf6250baa03801ab3a498ec6
Ihor Cherkes <ihor.cherkes@techmagic.co>
2022-09-02 12:38:31 +0300

    new secret add

--- Development/templates/hcp_secrets.tf
+++ Development/templates/hcp_secrets.tf
@@ -4,11 +4,11 @@ resource "vault_mount" "test" {
   type = "kv"
 }
 
-# # Enable Transit secrets engine at 'transit'
-# resource "vault_mount" "transit" {
-#   path = "transit"
-#   type = "transit"
-# }
+# Enable Transit secrets engine at 'transit'
+resource "vault_mount" "transit" {
+  path = "transit"
+  type = "transit"
+}
 
 # # Creating an encryption key named 'payment'
 # resource "vault_transit_secret_backend_key" "key" {
